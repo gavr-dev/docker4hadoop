14a838f6cebcbbd12c1511bf86154d5898592a46
Aleksandr Gavrilenko <agavrilenko2.0@gmail.com>
2021-03-07 07:04:22 +0100

    More configs for HDFS and YARN

--- docker/krb5/conf/init.sh
+++ docker/krb5/conf/init.sh
@@ -13,6 +13,17 @@ kadmin.local -q "addprinc -pw 123456 root@LC.CLUSTER"
 /usr/sbin/kadmind
 /usr/sbin/httpd
 
+mkdir /opt/keytabs
+
+kadmin -w 123456 -q "addprinc -randkey om"
+kadmin -w 123456 -q "ktadd -k /opt/keytabs/om.keytab om@LC.CLUSTER"
+kadmin -w 123456 -q "addprinc -randkey scm"
+kadmin -w 123456 -q "ktadd -k /opt/keytabs/scm.keytab scm@LC.CLUSTER"
+kadmin -w 123456 -q "addprinc -randkey dn"
+kadmin -w 123456 -q "ktadd -k /opt/keytabs/dn.keytab dn@LC.CLUSTER"
+kadmin -w 123456 -q "addprinc -randkey recon"
+kadmin -w 123456 -q "ktadd -k /opt/keytabs/recon.keytab recon@LC.CLUSTER"
+
 /usr/sbin/sshd
 echo -e "123456\n123456\n" | passwd root
 /usr/sbin/sshd
