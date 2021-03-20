c00243755326ec5549724d7a4f6a4385f23390c6
Aleksandr Gavrilenko <agavrilenko2.0@gmail.com>
2021-03-20 05:15:44 +0100

    Debug all images

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-init
+## Building Images
+```shell
+docker-compose -f docker-vm.yml build krb5 core hadoop-base hadoop-namenode hadoop-datanode hadoop-hive hadoop-nodemanager hadoop-resourcemanager hadoop-historyserver 
+```
+## Launch Options
+### HDFS Only
+```shell
+docker-compose -f docker-compose.yml up -d krb5 hdfs-nn hdfs-dn
+```
+### HDFS & Hive
+```shell
+docker-compose -f docker-compose.yml up -d krb5 hdfs-nn hdfs-dn hive-server hive-metastore hive-metastore-db
+```
+### HDFS & Spark
+```shell
+docker-compose -f docker-compose.yml up -d krb5 hdfs-nn hdfs-dn resourcemanager nodemanager historyserver
+```
+### HDFS, Hive & Spark
+```shell
+docker-compose -f docker-compose.yml up -d krb5 hdfs-nn hdfs-dn hive-server hive-metastore hive-metastore-db resourcemanager nodemanager historyserver
+```
--- docker/hadoop/base/init.sh
+++ docker/hadoop/base/init.sh
@@ -17,4 +17,35 @@ adduser hdfs
 echo -e "123456\n123456\n" | passwd hdfs
 chown hdfs:hdfs $HADOOP_HOME/keytabs/hdfs.keytab
 
+kadmin -w 123456 -q "ktadd -k $HADOOP_HOME/keytabs/yarn.keytab HTTP/$HOST@LC.CLUSTER"
+kadmin -w 123456 -q "addprinc -randkey yarn/$HOST"
+kadmin -w 123456 -q "ktadd -k $HADOOP_HOME/keytabs/yarn.keytab yarn/$HOST@LC.CLUSTER"
+adduser yarn
+echo -e "123456\n123456\n" | passwd yarn
+chown yarn:yarn $HADOOP_HOME/keytabs/yarn.keytab
+
+kadmin -w 123456 -q "addprinc -randkey hive/$HOST"
+kadmin -w 123456 -q "ktadd -k $HADOOP_HOME/keytabs/hive.keytab hive/$HOST@LC.CLUSTER"
+adduser hive
+echo -e "123456\n123456\n" | passwd hive
+chown hive:hive $HADOOP_HOME/keytabs/hive.keytab
+
+kadmin -w 123456 -q "ktadd -k $HADOOP_HOME/keytabs/spark.keytab HTTP/$HOST@LC.CLUSTER"
+kadmin -w 123456 -q "addprinc -randkey spark/$HOST"
+kadmin -w 123456 -q "ktadd -k $HADOOP_HOME/keytabs/spark.keytab spark/$HOST@LC.CLUSTER"
+adduser spark
+echo -e "123456\n123456\n" | passwd spark
+chown spark:spark $HADOOP_HOME/keytabs/spark.keytab
+
 sshpass -p '123456' scp -o StrictHostKeyChecking=no root@krb5.lc.cluster:/opt/*.keytab /opt/hadoop-2.6.5/keytabs/
+
+
+keytool -genkey -noprompt -dname "CN=$HOSTNAME, OU=AA, O=AA, L=AA, S=AA, C=AA" -alias "$HOSTNAME" -keystore "keystore.jks" -storepass "12345678" -keypass "12345678"
+cp $HADOOP_HOME/keytabs/hdfs.keytab /root/hadoop.keytab
+cp keystore.jks /root/.keystore
+echo "12345678" > /root/hadoop-http-auth-signature-secret
+chmod -R 777 /root
+
+
+cp $HADOOP_CONF_DIR/yarn-site.xml.template_yarn $HADOOP_CONF_DIR/yarn-site.xml;
+cp $HADOOP_CONF_DIR/mapred-site.xml.template_yarn $HADOOP_CONF_DIR/mapred-site.xml
